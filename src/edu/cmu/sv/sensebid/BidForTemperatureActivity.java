de25799e5d9a4fce909c40f5ca78ee5658358e0c
Anubhav Aeron <anubhav.aeron@sv.cmu.edu>
2013-11-10 12:13:50 -0800

    able to retrieve values from the screen

--- src/edu/cmu/sv/sensebid/BidForTemperatureActivity.java
+++ src/edu/cmu/sv/sensebid/BidForTemperatureActivity.java
@@ -1,7 +1,71 @@
 package edu.cmu.sv.sensebid;
 
+import com.google.gson.JsonObject;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class BidForTemperatureActivity extends Activity {
+	
 
+	int coins;
+	Button buttonBid, buttonTime;
+	TextView display;
+	EditText room, day_1, month_1, hour_1, min_1, temp_1, coins_1;
+	@Override
+	public final void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		Log.d("Test", "I am here");
+		
+//		
+		
+		
+		buttonBid = (Button) findViewById(R.id.buttonBid);
+		// display = (TextView) findViewById(R.id.tvDisplay);
+		//room, day_1, month_1, hour_1, min_1, temp_1, coins_1
+		room = (EditText) findViewById(R.id.etRooms);
+		day_1 = (EditText) findViewById(R.id.etDay);
+		month_1 = (EditText) findViewById(R.id.etMonth);
+		hour_1 = (EditText) findViewById(R.id.etHours);
+		min_1 = (EditText) findViewById(R.id.etMinutes);
+		temp_1 = (EditText) findViewById(R.id.etTemp);
+		coins_1 = (EditText) findViewById(R.id.etCoins);
+
+
+		
+
+		
+		buttonBid.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+				JsonObject bidData = new JsonObject();
+
+				bidData.addProperty("room", room.getText().toString());
+				bidData.addProperty("day", day_1.getText().toString());
+				bidData.addProperty("month", month_1.getText().toString());
+				bidData.addProperty("hours", hour_1.getText().toString());
+				bidData.addProperty("minutes", min_1.getText().toString());
+				bidData.addProperty("temp", temp_1.getText().toString());
+				bidData.addProperty("coins", coins_1.getText().toString());
+				
+				Log.d("Test", bidData.toString());
+				
+				//new BidTemperatureController().execute(bidData.toString());
+
+			}
+		});
+	
+
+	}
 }
--- src/edu/cmu/sv/sensebid/BidTemperatureController.java
+++ src/edu/cmu/sv/sensebid/BidTemperatureController.java
@@ -1,5 +1,73 @@
-package edu.cmu.sv.sensebid;
-
-public class BidTemperatureController {
-
-}
+//package edu.cmu.sv.sensebid;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.io.OutputStream;
+//import java.io.OutputStreamWriter;
+//import java.io.Writer;
+//import java.net.HttpURLConnection;
+//import java.net.URL;
+//import java.util.Iterator;
+//import java.util.List;
+//
+//import android.os.AsyncTask;
+//import android.util.Log;
+//
+//import com.google.gson.Gson;
+//import com.google.gson.JsonObject;
+//
+//public class BidTemperatureController extends
+//		AsyncTask<String, String, Long> {
+//
+//	private static final String URL = "ip.address.Ira";
+//
+//
+//	protected String doInBackground(String... arguments) {
+//
+//		String urlStr = arguments[0];
+//		String jsonString = arguments[1];
+//		
+//		URL url = new URL(urlStr);
+//		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//		conn.setRequestMethod("POST");
+//		conn.setRequestProperty("Content-Type", "application/json");
+//		conn.setRequestProperty("Accept", "application/json");
+//		conn.setDoOutput(true);
+//
+//		OutputStream out = null;
+//		Writer writer = null;
+//		try {
+//			out = conn.getOutputStream();
+//			writer = new OutputStreamWriter(out, "UTF-8");
+//
+//			writer.write((new Gson()).toJson(jsonString));
+//		} finally {
+//			writer.close();
+//			out.close();
+//		}
+//
+//		if (conn.getResponseCode() != 200) {
+//			throw new IOException(conn.getResponseMessage());
+//		}
+//		StringBuilder sb;
+//		BufferedReader rd = null;
+//		try {
+//			rd = new BufferedReader(
+//					new InputStreamReader(conn.getInputStream()));
+//			sb = new StringBuilder();
+//			String line;
+//			while ((line = rd.readLine()) != null) {
+//				sb.append(line);
+//			}
+//		} finally {
+//			rd.close();
+//		}
+//
+//		conn.disconnect();
+//		return sb.toString();
+//	}
+//
+//
+//
+//}
