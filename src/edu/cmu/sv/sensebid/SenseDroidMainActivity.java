3c014fe9c357467f75074b3a54f90e98b7713720
Anubhav Aeron <anubhav.aeron@sv.cmu.edu>
2013-11-20 19:47:26 -0800

    Changes to view p_1

--- src/edu/cmu/sv/sensebid/SenseDroidMainActivity.java
+++ src/edu/cmu/sv/sensebid/SenseDroidMainActivity.java
@@ -1,21 +1,36 @@
 package edu.cmu.sv.sensebid;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.ListView;
 
 public class SenseDroidMainActivity extends Activity {
 
 	@Override
 	public final void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+
+		
 		Intent i = new Intent(this, SensorDataPublisherService.class);
 		startActivity(i);
 
 		Intent j = new Intent(this, BidForTemperatureActivity.class);
 		startActivity(j);
+		
+		setContentView(R.layout.activity_main_screen);
+		
+		final ListView listview = (ListView) findViewById(R.id.listview);
+		
+	    //getting arraylist from Emi
+	    }
+		
+		final ArrayAdapter adapter = new ArrayAdapter(this,
+		        android.R.layout.simple_list_item_1, list);
+		    listview.setAdapter(adapter);
+
 	}
 
 }
